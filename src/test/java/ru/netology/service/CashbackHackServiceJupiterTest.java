fdf21202f8c40a06cd9a9e8ae5b412f8db771177
Grigoriy Gamarin <gam110191@gmail.com>
2020-04-30 19:22:17 +0300

    junit4-platform branch added

---
+++ src/test/java/ru/netology/service/CashbackHackServiceJupiterTest.java
@@ -0,0 +1,43 @@
+package ru.netology.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CashbackHackServiceJupiterTest {
+    CashbackHackService service = new CashbackHackService();
+
+    @Test
+    public void shouldReturn1000For0() {
+        int amount = 0;
+        int actual = service.remain(amount);
+        int expected = 1000;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldReturn999For1001() {
+        int amount = 1001;
+        int actual = service.remain(amount);
+        int expected = 999;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldReturn0For1000() {
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldReturn100For900() {
+        int amount = 100;
+        int actual = service.remain(amount);
+        int expected = 900;
+        assertEquals(expected, actual);
+    }
+
+
+}
